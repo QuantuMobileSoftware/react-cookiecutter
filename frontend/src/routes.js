a137cd9b7d2d1cb8887f837930d06fe03f0d9c69
vvav3 <vvav3@pop-os.localdomain>
2019-01-14 14:18:20 +0200

    Routes changes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import { Router } from "@reach/router";
+import { Router, Redirect } from "@reach/router";
 
 const SignInPage = React.lazy(() => import("./modules/auth/pages/SignInPage"));
 const SignUpPage = React.lazy(() => import("./modules/auth/pages/SignUpPage"));
@@ -7,15 +7,21 @@ const Home = () => <div>Home</div>;
 const About = () => <div>About</div>;
 const NotFound = () => <div>Not Found</div>;
 
-const restricted = <Home path="/" />;
-const shared = [<NotFound key="1" default />, <About key="2" path="about" />];
-const auth = [<SignInPage key="1" path="sign-in" />, <SignUpPage key="2" path="sign-up" />];
+const restricted = [<Home path="/" />];
+const shared = [<NotFound default />, <About path="about" />];
+const auth = [
+  <Redirect from="/" to="sign-in" />,
+  <SignInPage path="sign-in" />,
+  <SignUpPage path="sign-up" />
+];
+
+const mapLinks = arr => arr.map((item, i) => ({ ...item, key: i }));
 
 const routes = authorized => (
   <Suspense fallback={<div>Loading...</div>}>
     <Router>
-      {authorized ? restricted : auth}
-      {shared}
+      {authorized ? mapLinks(restricted) : mapLinks(auth)}
+      {mapLinks(shared)}
     </Router>
   </Suspense>
 );
